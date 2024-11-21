d23eb1681e02113474b0ff246cf479d668077946
Uzair Siddiqui <uzairs2@illinois.edu>
2024-11-20 18:50:19 -0600

    update

--- Project/fashion-trend/src/App.js
+++ Project/fashion-trend/src/App.js
@@ -5,7 +5,7 @@ import React from 'react'
 function App() {
   return (
     <div className="App">
-      <title> className="App-header"
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +18,7 @@ function App() {
         >
           Learn Reactttttt
         </a>
-      </title>
+      </header>
     </div>
   );
 }
