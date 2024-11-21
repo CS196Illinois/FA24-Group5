129d3a9a55355ca76acbb11ed9bfb16b298d1d05
Uzair Siddiqui <uzairs2@illinois.edu>
2024-11-20 18:42:20 -0600

    inital frontend

--- Project/fashion-trend/src/App.js
+++ Project/fashion-trend/src/App.js
@@ -5,7 +5,7 @@ import React from 'react'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <title> className="App-header"
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,9 +16,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactttttt
         </a>
-      </header>
+      </title>
     </div>
   );
 }
