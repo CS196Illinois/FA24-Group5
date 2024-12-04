467a0637b68ad5ec66be72caa085574a250b79fd
anshimathur0325 <anshimathur0325@gmail.com>
2024-12-04 15:44:37 -0600

    edit changes

--- Project/fashion-trend/src/App.js
+++ Project/fashion-trend/src/App.js
@@ -1,27 +1,3 @@
-<<<<<<< Updated upstream
-import logo from './logo.svg';
-import './App.css';
-import React from 'react'
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
@@ -75,7 +51,6 @@ function App() {
         </Routes>
       </div>
     </BrowserRouter>
->>>>>>> Stashed changes
   );
 }
 
